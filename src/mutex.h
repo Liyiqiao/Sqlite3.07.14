0dad06e8db6c0bd66b128be3c3c91560e4792935
Liyiqiao <504738170@qq.com>
2015-12-21 22:11:01 +0800

    杜丹琪-12.21-mutex.h-update

--- src/mutex.h
+++ src/mutex.h
@@ -37,10 +37,23 @@
 **
 **   SQLITE_MUTEX_W32          For multi-threaded applications on Win32.
 */
-#if !SQLITE_THREADSAFE
+/*弄清楚要使用的是哪个版本的code，有以下选择：
+** SQLITE_MUTEX_OMIT          不实现互斥锁逻辑，当声明了这个宏，在启动阶段互斥锁实现代码不能被覆盖
+**
+** SQLITE_MUTEX_NOOP           适用于单线程应用，不提供互斥功能，但是当声明了这个宏，在启动阶段互斥锁实现代码能被覆盖
+**
+**SQLITE_MUTEX_PTHREADS        适用于Unix系统中的多线程应用
+**
+** SQLITE_MUTEX_W32            适用于Win32系统的多线程应用
+
+/*
+下面是对上述规则的规定
+*/
+
+#if !SQLITE_THREADSAFE    /*如果是非线程安全状态，则互斥锁只声明，不实现具体的逻辑*/
 # define SQLITE_MUTEX_OMIT
 #endif
-#if SQLITE_THREADSAFE && !defined(SQLITE_MUTEX_NOOP)
+#if SQLITE_THREADSAFE && !defined(SQLITE_MUTEX_NOOP)       /*如果是线程安全状态，并且没有定义单线程应用的宏**/
 #  if SQLITE_OS_UNIX
 #    define SQLITE_MUTEX_PTHREADS
 #  elif SQLITE_OS_WIN
@@ -53,6 +66,7 @@
 #ifdef SQLITE_MUTEX_OMIT
 /*
 ** If this is a no-op implementation, implement everything as macros.
+**如果以下的方法没有给出具体的实现，则用宏代替它实现它的功能，其实这里实际上就是给函数一个默认的返回值了
 */
 #define sqlite3_mutex_alloc(X)    ((sqlite3_mutex*)8)
 #define sqlite3_mutex_free(X)
